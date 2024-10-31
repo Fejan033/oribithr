cc1d395e856ecf194942129d4e62ef9106860d80
Mohammed <fejan033@gmail.com>
2024-10-31 12:01:14 +0530

    Req_Lib_Added

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit', // Ensure JIT mode is enabled
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
